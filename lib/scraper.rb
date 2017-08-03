20cadfa88b49dfe7090a209b2bfcd70344423863
Robert Peckham <peckham.rob@gmail.com>
2017-08-03 10:21:37 +0000

    added Scraper.scrape_region

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,32 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_region(region_url)
-    regions = []
-    home_page = Nokogiri::HTML(open(region_url)) #http://hpdejunkie.com/
+  # def initialize
+  # end
 
-    region = home_page.css('div.twleve columns')
+  def self.scrap_map
+
   end
+
+  def self.scrape_region
+    regions = []
+    home_page = Nokogiri::HTML(open("http://hpdejunkie.com/"))
+    home_page.css('div.columns.three').each do |choose_region|
+      choose_region.css('.button.btn-red').each do |map|
+        regions << map.css('.map-text').text
+      end
+    end
+    puts "Choose your region"
+    puts "#{regions.each_with_index}"
+    region_choice = gets.strip
+
+    #Hard code this instead. Regions won't change. have cli list regions and use if/else statement to link to region chosen, then start scraping.
+
+  end
+
+  scraper = Scraper.new
 
 end
